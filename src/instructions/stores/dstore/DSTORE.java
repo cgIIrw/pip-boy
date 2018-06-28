80e3cbe34adeff61f24edeeb6b0d67fcfc698e5d
cgIIrw <chenguo_jay@163.com>
2018-06-28 11:52:04 +0800

    complete dstore

---
+++ src/instructions/stores/dstore/DSTORE.java
@@ -0,0 +1,19 @@
+package instructions.stores.dstore;
+
+import instructions.base.Index8Instruction;
+import rtda.Myframe;
+import rtda.Myobject;
+
+public class DSTORE extends Index8Instruction {
+    @Override
+    public void execute(Myframe frame) {
+        dstore(frame, index);
+
+    }
+
+    public static void dstore(Myframe myframe, int index) {
+//        Myobject ref = myframe.getLocalVars().getRef(index);
+        double ref = myframe.getOperandStack().popDouble();
+        myframe.getLocalVars().setDouble(index, ref);
+    }
+}
