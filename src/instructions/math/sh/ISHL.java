6c429c719492048ca6f9ab05a3089acbefea780b
cgIIrw <chenguo_jay@163.com>
2018-06-28 17:09:13 +0800

    complete instructions/math/sh

---
+++ src/instructions/math/sh/ISHL.java
@@ -0,0 +1,17 @@
+package instructions.math.sh;
+
+import instructions.base.NoOperandsInstruction;
+import rtda.Myframe;
+import rtda.OperandStack;
+
+public class ISHL extends NoOperandsInstruction {
+    @Override
+    public void execute(Myframe frame) {
+        OperandStack stack = frame.getOperandStack();
+        int i01 = stack.popInt();
+        int i02 = stack.popInt();
+        int s = i01 & 0x1f;
+        int result = i02 << s;
+        stack.pushInt(result);
+    }
+}
