b18c3ee9645f6f0938b74cffa32b97c7387c58f5
cgIIrw <chenguo_jay@163.com>
2018-07-02 11:06:38 +0800

    add rtda/heap/MethodRef

---
+++ src/rtda/heap/MethodRef.java
@@ -0,0 +1,18 @@
+package rtda.heap;
+
+import classfile.ConstantInfo;
+import classfile.constantInfos.ConstantMethodrefInfo;
+
+public class MethodRef extends MemberRef {
+    public MethodRef(RuntimeConstantPool runtimeConstantPool,  ConstantMethodrefInfo methodrefInfo) {
+        super(runtimeConstantPool);
+        copyMemberRefInfo(methodrefInfo);
+    }
+
+    @Override
+    void copyMemberRefInfo(ConstantInfo info) {
+        className = ((ConstantMethodrefInfo)info).getClassName();
+        name = ((ConstantMethodrefInfo)info).getNameAndDescriptor()[0];
+        descriptor = ((ConstantMethodrefInfo)info).getNameAndDescriptor()[1];
+    }
+}
