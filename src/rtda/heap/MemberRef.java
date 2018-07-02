8693cb375aae3265ee476989b038d8d71ec93599
cgIIrw <chenguo_jay@163.com>
2018-07-02 11:24:29 +0800

    add rtda/heap/MemberRef

--- src/rtda/heap/MemberRef.java
+++ src/rtda/heap/MemberRef.java
@@ -1,4 +1,23 @@
 package rtda.heap;
 
-public class MemberRef {
+import classfile.ConstantInfo;
+
+public abstract class MemberRef extends SymRef{
+
+    String name;
+    String descriptor;
+
+    public MemberRef(RuntimeConstantPool runtimeConstantPool) {
+        super(runtimeConstantPool);
+    }
+
+    abstract void copyMemberRefInfo(ConstantInfo info);
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescriptor() {
+        return descriptor;
+    }
 }
