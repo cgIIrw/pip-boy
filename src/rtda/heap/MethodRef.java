c08f3a11310b735789b14fed4e5991eba87cc1ac
cgIIrw <chenguo_jay@163.com>
2018-07-05 15:07:17 +0800

    add resolveMethodRef to MethodRef

--- src/rtda/heap/MethodRef.java
+++ src/rtda/heap/MethodRef.java
@@ -4,6 +4,8 @@ import classfile.ConstantInfo;
 import classfile.constantInfos.ConstantMethodrefInfo;
 
 public class MethodRef extends MemberRef {
+    private MyMethod method;
+
     public MethodRef(RuntimeConstantPool runtimeConstantPool,  ConstantMethodrefInfo methodrefInfo) {
         super(runtimeConstantPool);
         copyMemberRefInfo(methodrefInfo);
@@ -14,5 +16,68 @@ public class MethodRef extends MemberRef {
         setClassName(((ConstantMethodrefInfo)info).getClassName());
         setName(((ConstantMethodrefInfo)info).getNameAndDescriptor()[0]);
         setDescriptor(((ConstantMethodrefInfo)info).getNameAndDescriptor()[1]);
+    }
+
+    public MyMethod resolvedMethod() {
+        if (method == null) {
+            resolveMethodRef();
+        }
+        return this.method;
+    }
+
+    public void resolveMethodRef() {
+        Myclass d = this.getRuntimeConstantPool().getMyclass();
+        Myclass c = this.resolvedClass();
+
+        if (c.isInterface()) {
+            throw new IncompatibleClassChangeError();
+        }
+
+        MyMethod method = lookupMethod(c, this.getName(), this.getDescriptor());
+
+        if (method == null) {
+            throw new NoSuchMethodError();
+        }
+
+        if (!method.isAccessibleTo(d)) {
+            throw new IllegalAccessError();
+        }
+
+        this.method = method;
+    }
+
+    public MyMethod lookupMethod(Myclass myclass, String name, String descriptor) {
+        MyMethod method = lookupMethodInClass(myclass, name, descriptor);
+        if (method == null) {
+            method = lookupMethodInInterfaces(myclass.getInterfaces(), name, descriptor);
+        }
+        return method;
+    }
+
+    MyMethod lookupMethodInClass(Myclass myclass, String name, String descriptor) {
+        for (Myclass c = myclass; c != null; c = c.getSuperClass()) {
+            for (MyMethod method : c.getMethods()) {
+                if (method.getName().equals(name) && method.getDescriptor().equals(descriptor)) {
+                    return method;
+                }
+            }
+        }
+        return null;
+    }
+
+    public MyMethod lookupMethodInInterfaces(Myclass[] ifaces, String name, String descriptor) {
+        for (Myclass iface : ifaces) {
+            for (MyMethod method : iface.getMethods()) {
+                if (method.getName().equals(name) && method.getDescriptor().equals(descriptor)) {
+                    return method;
+                }
+            }
+
+            MyMethod method = lookupMethodInInterfaces(iface.getInterfaces(), name, descriptor);
+            if (method != null) {
+                return method;
+            }
+        }
+        return null;
     }
 }
