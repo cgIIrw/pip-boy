f4eec39556fd6f27c8150106deb5ef34b9aae91e
cgIIrw <chenguo_jay@163.com>
2018-07-24 17:58:52 +0800

    Add comments to MethodRef & InterfaceMethodRef

--- src/rtda/heap/InterfaceMethodRef.java
+++ src/rtda/heap/InterfaceMethodRef.java
@@ -3,6 +3,9 @@ package rtda.heap;
 import classfile.ConstantInfo;
 import classfile.constantInfos.ConstantInterfaceMethodrefInfo;
 
+/**
+ * 接口方法引用类，并提供解析方法
+ */
 public class InterfaceMethodRef extends MemberRef {
     private MyMethod method;
 
@@ -20,6 +23,8 @@ public class InterfaceMethodRef extends MemberRef {
     }
 
     public MyMethod resolvedInterfaceMethod() {
+
+        // 判断是否缓存有已经解析过的方法，没有则进行解析
         if (this.method == null) {
             resolvedInterfaceMethodRef();
         }
@@ -27,31 +32,43 @@ public class InterfaceMethodRef extends MemberRef {
     }
 
     public void resolvedInterfaceMethodRef() {
+
+        // 当前代码所处的类d
         Myclass d = this.getRuntimeConstantPool().getMyclass();
+
+        // 要解析的非接口方法所属的类或接口c
         Myclass c = this.resolvedClass();
 
+        // 判断c是否是接口，如果不是则抛出IncompatibleClassChangeError异常
         if (!c.isInterface()) {
             throw new IncompatibleClassChangeError();
         }
 
         MyMethod method = lookupInterfaceMethod(c, this.getName(), this.getDescriptor());
+
+        // 执行到这一步如果仍然没有查找到符合条件的方法，则抛出NoSuchMethodError异常
         if (method == null) {
             throw new NoSuchMethodError();
         }
 
-        if (!method.isAccessibleTo(d)) {
-            throw new IllegalAccessError();
-        }
+        // 接口中所有方法默认都是public，不存在访问权限问题
+//        if (!method.isAccessibleTo(d)) {
+//            throw new IllegalAccessError();
+//        }
 
         this.method = method;
     }
 
     public MyMethod lookupInterfaceMethod(Myclass iface, String name, String descriptor) {
+
+        // 在接口中查找是否有简单名和描述符都与目标相匹配的方法，如果有，返回这个方法的直接引用，查找结束
         for (MyMethod method : iface.getMethods()) {
             if (method.getName().equals(name) && method.getDescriptor().equals(descriptor)) {
                 return method;
             }
         }
-        return lookupMethodInInterfaces(iface.getInterfaces(), name, descriptor);
+
+        // 对接口的父接口递归查找简单名和描述符都与目标相匹配的方法，没查找到返回null
+        return MethodLookup.lookupMethodInInterfaces(iface.getInterfaces(), name, descriptor);
     }
 }
--- src/rtda/heap/MethodLookup.java
+++ src/rtda/heap/MethodLookup.java
@@ -3,6 +3,8 @@ package rtda.heap;
 public class MethodLookup {
 
     public static MyMethod lookupMethodInClass(Myclass myclass, String name, String descriptor) {
+
+        // 在myclass和它的父类中递归查找简单名和描述符都与目标相匹配的方法
         for (Myclass c = myclass; c != null; c = c.getSuperClass()) {
             for (MyMethod method : c.getMethods()) {
                 if (method.getName().equals(name) && method.getDescriptor().equals(descriptor)) {
@@ -14,6 +16,8 @@ public class MethodLookup {
     }
 
     public static MyMethod lookupMethodInInterfaces(Myclass[] ifaces, String name, String descriptor) {
+
+        // 传入的ifaces是接口列表，在它们以及它们的父接口递归查找简单名和描述符都与目标相匹配的方法
         for (Myclass iface : ifaces) {
             for (MyMethod method : iface.getMethods()) {
                 if (method.getName().equals(name) && method.getDescriptor().equals(descriptor)) {
