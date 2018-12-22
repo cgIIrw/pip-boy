cf50ed9317b93af3131566770d4c57d934585767
cgIIrw <chenguo_jay@163.com>
2018-12-22 15:20:01 +0800

    修改Cmd的printUsage方法，使其符合要求的打印逻辑

--- src/main/java/Cmd.java
+++ src/main/java/Cmd.java
@@ -4,7 +4,7 @@ import org.apache.commons.cli.*;
  * Created by yin on 18/4/2.
  */
 class Cmd {
-    public static CommandLine cmd(String[] args) throws ParseException {
+    static CommandLine cmd(String[] args) throws ParseException {
         Options options = new Options();
         options.addOption("help", false, "print help message");
         options.addOption("?", false, "print help message");
@@ -13,17 +13,11 @@ class Cmd {
         options.addOption("cp", "classpath", true, "classpath");
         //解析
         CommandLineParser paraer = new DefaultParser();
-        CommandLine cmd = paraer.parse(options, args);
-        return cmd;
+        return paraer.parse(options, args);
     }
 
-    public static void printUsage(String[] args) {
-        if (args.length > 0) {
-            System.out.printf("Usage: %s [-options] class [args...]\n", args[0]);
-        }
-        else {
-            System.out.println("Usage: arg [-options] class [args...]");
-        }
+    static void printUsage(String className) {
+        System.out.println("Usage: java " + className + " [-options] class [args...]");
     }
 }
 
