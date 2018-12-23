35862730dd529be861194e83d598c01eea0b7a61
cgIIrw <chenguo_jay@163.com>
2018-12-23 14:53:52 +0800

    修改Wildcard_Entry代码逻辑

--- src/main/java/classpath/Wildcard_Entry.java
+++ src/main/java/classpath/Wildcard_Entry.java
@@ -9,43 +9,50 @@ import java.util.List;
  * Created by yin on 18/4/2.
  */
 public class Wildcard_Entry implements Entry {
-    List<Entry> cp = new ArrayList<>();
+    private List<Entry> cp = new ArrayList<>();
 
-
-    public Wildcard_Entry(String path) {
+    Wildcard_Entry(String path) {
 
         String baseDir = path.substring(0, path.length() - 1);
         File file = new File(baseDir);
+        if (!file.exists())
+            throw new RuntimeException("不存在当前路径！");
         File[] files = file.listFiles();
         if (files == null) {
-            throw new RuntimeException("没有获得目录下的子文件和子目录!");
+            throw new RuntimeException("没有获得目录下的子文件和子目录！");
         }
         for (File f : files) {
-            if ((f.isFile() && f.getName().endsWith(".jar")) || (f.isFile() && f.getName().endsWith(".JAR"))) {
-                Entry jarEntry = new Zip_Entry(baseDir + f.getName());
-                cp.add(jarEntry);
+            // 如果当前遍历的目录下的实体不是文件，则跳过单次遍历
+            if (!f.isFile())
+                continue;
+
+            if (f.getName().endsWith(".jar") || f.getName().endsWith(".JAR")) {
+                cp.add(new Zip_Entry(baseDir + f.getName()));
             }
         }
+        // 当前目录下有可能存在目标文件，且同一目录下不会有同名的多个文件
+        // 所以添加一个Dir_Entry对象处理
+        cp.add(new Dir_Entry(baseDir));
     }
 
     @Override
     public byte[] readClass(String className) throws IOException {
         for (Entry entry : cp) {
             if (entry.readClass(className) != null) {
-                return entry.readClass(className);   // 不考虑多过路径返回多个结果
+                // 不考虑多个路径返回多个结果
+                return entry.readClass(className);
             }
         }
         return null;
     }
 
     @Override
-    public String string() {
+    public String getString() {
         StringBuilder sb = new StringBuilder();
         for (Entry entry : cp) {
-            sb.append(entry.string());
+            sb.append(entry.getString());
             sb.append(Entry.pathListSeparator);
         }
         return sb.toString();
-
     }
 }
