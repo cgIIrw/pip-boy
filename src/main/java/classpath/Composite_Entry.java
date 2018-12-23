811b0f3c303d83c5d87dfea2691f8509cdf6f229
cgIIrw <chenguo_jay@163.com>
2018-12-23 13:08:17 +0800

    规范Composite_Entry

--- src/main/java/classpath/Composite_Entry.java
+++ src/main/java/classpath/Composite_Entry.java
@@ -11,50 +11,33 @@ import java.util.List;
 public class Composite_Entry implements Entry {
     private List<Entry> compositeEntry;
 
-//    public Composite_Entry() {
-//        compositeEntry = new ArrayList<>();
-//    }
-
     public Composite_Entry(String path) {
         compositeEntry = new ArrayList<>();
-        for(String p: path.split(Entry.pathListSeparator)) {
+        for (String p : path.split(Entry.pathListSeparator)) {
             compositeEntry.add(NewEntry.newEntry(p));
         }
     }
+
     @Override
     public byte[] readClass(String className) throws IOException {
-        for (Entry entry: compositeEntry) {
+        for (Entry entry : compositeEntry) {
             if (entry.readClass(className) != null) {
-                return entry.readClass(className);   // 不考虑多过路径返回多个结果
+
+                // 不考虑多个路径返回多个结果
+                return entry.readClass(className);
             }
         }
-//        Iterator<Entry> itr = compositeEntry.iterator();
-//        while(itr.hasNext()) {
-//            Entry it = itr.next();
-//            if(it.readClass(className) != null) {
-//                return it.readClass(className);
-//            }
-//        }
         return null;
     }
 
     @Override
-    public String string() {
+    // 将所有路径的绝对路径拼接在一起返回
+    public String getString() {
         StringBuilder sb = new StringBuilder();
-        for (Entry entry: compositeEntry) {
-            sb.append(entry.string());
+        for (Entry entry : compositeEntry) {
+            sb.append(entry.getString());
             sb.append(Entry.pathListSeparator);
         }
-//        Iterator<Entry> strs = compositeEntry.iterator();
-//        while(strs.hasNext()) {
-//            Entry it = strs.next();
-//            sb.append(it.string());
-//            sb.append(Entry.pathListSeparator);
-//        }
         return sb.toString();
     }
-
-//    public void addEntry(Entry entry) {
-//        compositeEntry.add(entry);
-//    }
 }
