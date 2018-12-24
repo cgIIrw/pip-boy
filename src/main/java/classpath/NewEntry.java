ec5256cf9452d4942517fb322359eb8b6c660c7b
cgIIrw <chenguo_jay@163.com>
2018-12-24 08:25:54 +0800

    更新NewEntry

--- src/main/java/classpath/NewEntry.java
+++ src/main/java/classpath/NewEntry.java
@@ -1,4 +1,26 @@
 package classpath;
 
-public class NewEntry {
+/**
+ * Created by yin on 18/4/2.
+ */
+// 用来创建各种路径实例的类
+class NewEntry {
+
+    public static Entry newEntry(String path) {
+        if (path != null) {
+            if (path.contains(Entry.pathListSeparator)) {
+                return new Composite_Entry(path);
+            }
+            if (path.endsWith("*")) {
+                return new Wildcard_Entry(path);
+            }
+            if (path.endsWith(".jar") || path.endsWith(".JAR") ||
+                    path.endsWith(".zip") || path.endsWith(".ZIP")) {
+                return new Zip_Entry(path);
+            }
+            return new Dir_Entry(path);
+        } else {
+            throw new RuntimeException("输入错误!");
+        }
+    }
 }
