d0996d7fdcfe186d535e9c334483703436252330
cgIIrw <chenguo_jay@163.com>
2018-12-25 09:26:35 +0800

    修改常量池表工厂ConstantInfoFactory

---
+++ src/main/java/classfile/constantpool/ConstantInfoFactory.java
@@ -0,0 +1,66 @@
+package classfile.constantpool;
+
+import classfile.constantpool.constantInfos.*;
+import classfile.utils.ClassReader;
+
+public class ConstantInfoFactory {
+    public static final int CONSTANT_Class = 7;
+    public static final int CONSTANT_Fieldref = 9;
+    public static final int CONSTANT_Methodref = 10;
+    public static final int CONSTANT_InterfaceMethodref = 11;
+    public static final int CONSTANT_String = 8;
+    public static final int CONSTANT_Integer = 3;
+    public static final int CONSTANT_Float = 4;
+    public static final int CONSTANT_Long = 5;
+    public static final int CONSTANT_Double = 6;
+    public static final int CONSTANT_NameAndType = 12;
+    public static final int CONSTANT_Utf8 = 1;
+    public static final int CONSTANT_MethodHandle = 15;
+    public static final int CONSTANT_MethodType = 16;
+    public static final int CONSTANT_InvokeDynamic = 18;
+
+    public static ConstantInfo readConstantInfo(ClassReader reader, ConstantPool cp) {
+        // 先求出tag
+        int tag = reader.readUint8();
+        // 根据tag求出常量池中表的对象
+        ConstantInfo c = newConstantInfo(tag, cp);
+        // 调用对象的readInfo方法
+        c.readInfo(reader);
+        return c;
+    }
+
+    public static ConstantInfo newConstantInfo(int tag, ConstantPool cp) {
+        switch (tag) {
+            case CONSTANT_Utf8:
+                return new ConstantUtf8Info();
+            case CONSTANT_Integer:
+                return new ConstantIntegerInfo();
+            case CONSTANT_Float:
+                return new ConstantFloatInfo();
+            case CONSTANT_Long:
+                return new ConstantLongInfo();
+            case CONSTANT_Double:
+                return new ConstantDoubleInfo();
+            case CONSTANT_String:
+                return new ConstantStringInfo(cp);
+            case CONSTANT_Class:
+                return new ConstantClassInfo(cp);
+            case CONSTANT_Fieldref:
+                return new ConstantFieldrefInfo(cp);
+            case CONSTANT_Methodref:
+                return new ConstantMethodrefInfo(cp);
+            case CONSTANT_InterfaceMethodref:
+                return new ConstantInterfaceMethodrefInfo(cp);
+            case CONSTANT_NameAndType:
+                return new ConstantNameAndTypeInfo();
+            case CONSTANT_MethodType:
+                return new ConstantMethodTypeInfo();
+            case CONSTANT_MethodHandle:
+                return new ConstantMethodHandleInfo();
+            case CONSTANT_InvokeDynamic:
+                return new ConstantInvokeDynamicInfo();
+            default:
+                throw new RuntimeException("常量池tag错误！");
+        }
+    }
+}
