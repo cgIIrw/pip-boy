3592e1ced295a0b76de6fe7a95b4280f018df680
cgIIrw <chenguo_jay@163.com>
2019-01-20 21:24:19 +0800

    完成数组长度指令ARRAY_LENGTH的编写

--- src/main/java/instructions/references/ARRAY_LENGTH.java
+++ src/main/java/instructions/references/ARRAY_LENGTH.java
@@ -1,4 +1,28 @@
 package instructions.references;
 
-public class ARRAY_LENGTH {
+import instructions.base.NoOperandsInstruction;
+import rtda.heap.Instance_;
+import rtda.stack.OperandStack_;
+import rtda.stack.StackFrame_;
+
+public class ARRAY_LENGTH extends NoOperandsInstruction {
+    @Override
+    public void execute(StackFrame_ frame) {
+        OperandStack_ operandStack = frame.getOperandStack();
+        Instance_ ref = operandStack.popRef();
+        if (ref == null)
+            throw new NullPointerException();
+
+        int arrLen;
+        // 获取slots数组长度
+        int slotsLength = ref.getFields().getSlots().length;
+        // 如果数组是Double或者Long类型，则数组长度为slots长度的二分之一
+        if (ref.getClass_().getThisClassName().equals("[D") ||
+                ref.getClass_().getThisClassName().equals("[J")) {
+            arrLen = slotsLength / 2;
+        } else {
+            arrLen = slotsLength;
+        }
+        operandStack.pushInt(arrLen);
+    }
 }
