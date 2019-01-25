7bae156e6c23621164666c5ff378504889283345
cgIIrw <chenguo_jay@163.com>
2019-01-25 21:10:32 +0800

    删除过往对本地方法进行的钩子处理的代码

--- src/main/java/instructions/base/MethodInvokeLogic.java
+++ src/main/java/instructions/base/MethodInvokeLogic.java
@@ -33,8 +33,8 @@ public class MethodInvokeLogic {
             }
         }
 
-        if (method.isNative() && method.getName().equals("registerNatives")) {
-            thread.popStackFrame_();
-        }
+//        if (method.isNative() && method.getName().equals("registerNatives")) {
+//            thread.popStackFrame_();
+//        }
     }
 }
