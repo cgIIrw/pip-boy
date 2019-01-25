0ffe90a83b9a15f95950bfb4b0b127d71979d21a
cgIIrw <chenguo_jay@163.com>
2019-01-25 15:19:21 +0800

    feat 本地方法的注册与访问功能

--- src/main/java/native_/Registry.java
+++ src/main/java/native_/Registry.java
@@ -1,4 +1,29 @@
 package native_;
 
+import rtda.stack.StackFrame_;
+
+import java.util.HashMap;
+
 public class Registry {
+
+    private static HashMap<String, NativeMethod> register = new HashMap<>();
+
+    // 一一映射
+    public static void registerMethod(String className, String methodName, String methodDescriptor, NativeMethod nativeMethod) {
+        String key = className + "~" + methodName + "~" + methodDescriptor;
+        register.put(key, nativeMethod);
+    }
+
+    public NativeMethod findNativeMethod(String className, String methodName, String methodDescriptor) {
+        String key = className + "~" + methodName + "~" + methodDescriptor;
+        if (register.containsKey(key)) {
+            return register.get(key);
+        }
+        if (methodDescriptor.equals("()V") && methodName.equals("registerNatives")) {
+            return frame -> {
+                // do nothing
+            };
+        }
+        return null;
+    }
 }
