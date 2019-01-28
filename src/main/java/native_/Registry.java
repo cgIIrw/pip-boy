804de4ccf4e3862391f81ddaea23dca557a04d33
cgIIrw <chenguo_jay@163.com>
2019-01-28 19:38:21 +0800

    添加查找到registerNatives方法时的逻辑

--- src/main/java/native_/Registry.java
+++ src/main/java/native_/Registry.java
@@ -1,5 +1,7 @@
 package native_;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 
 public class Registry {
@@ -19,6 +21,26 @@ public class Registry {
         }
         if (methodDescriptor.equals("()V") && methodName.equals("registerNatives")) {
             return frame -> {
+                String[] temp = className.split("/");
+                String string = "";
+                for (int i = 0; i < temp.length - 1; i++) {
+                    string = string + temp[i] + ".";
+                }
+
+                // 将原本className的最后的简单名前添加一个“J”，在className前添加“native_.”
+                String name = "native_." + string + "J" + temp[temp.length - 1];
+
+                try {
+                    Class<?> c = Class.forName(name);
+                    for (Method method : c.getMethods()) {
+                        // 方法名以"registerNatives"结尾时，为要寻找的方法
+                        if (method.getName().endsWith("registerNatives"))
+                            // 静态方法，所以第一个参数为null
+                            method.invoke(null, className);
+                    }
+                } catch (ClassNotFoundException | IllegalAccessException | InvocationTargetException e) {
+                    e.printStackTrace();
+                }
                 // do nothing
 //                System.out.println(frame.getMethod_().getClass_().getThisClassName());
             };
