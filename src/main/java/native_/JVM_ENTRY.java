4de8f37ab80b1bb92c07cb8148b83dab7571ef6b
cgIIrw <chenguo_jay@163.com>
2019-01-28 19:37:00 +0800

    将本地方法声明在JVM_ENTRY接口下

--- src/main/java/native_/JVM_ENTRY.java
+++ src/main/java/native_/JVM_ENTRY.java
@@ -1,4 +1,49 @@
 package native_;
 
+import rtda.heap.Instance_;
+import rtda.stack.StackFrame_;
+
 public interface JVM_ENTRY {
+
+    class JVM_IHashCode implements NativeMethod {
+
+        @Override
+        public void execute(StackFrame_ frame) {
+            Instance_ thisRef = frame.getLocalVars().getRef(0);
+            int hashCode = thisRef.hashCode();
+            frame.getOperandStack().pushInt(hashCode);
+        }
+    }
+
+    class JVM_MonitorWait implements NativeMethod {
+
+        @Override
+        public void execute(StackFrame_ frame) {
+
+        }
+    }
+
+    class JVM_MonitorNotify implements NativeMethod {
+
+        @Override
+        public void execute(StackFrame_ frame) {
+
+        }
+    }
+
+    class JVM_MonitorNotifyAll implements NativeMethod {
+
+        @Override
+        public void execute(StackFrame_ frame) {
+
+        }
+    }
+
+    class JVM_Clone implements NativeMethod {
+
+        @Override
+        public void execute(StackFrame_ frame) {
+
+        }
+    }
 }
