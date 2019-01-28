29c60aa096cd09069f78650884a14def0efacd01
cgIIrw <chenguo_jay@163.com>
2019-01-28 19:39:49 +0800

    编写JNINativeMethod类用来存储参数，用来注册本地方法

--- src/main/java/native_/JNINativeMethod.java
+++ src/main/java/native_/JNINativeMethod.java
@@ -1,4 +1,25 @@
 package native_;
 
 public class JNINativeMethod {
+    private String methodName;
+    private String methodDescriptor;
+    private NativeMethod nativeMethod;
+
+    public JNINativeMethod(String methodName, String methodDescriptor, NativeMethod nativeMethod) {
+        this.methodName = methodName;
+        this.methodDescriptor = methodDescriptor;
+        this.nativeMethod = nativeMethod;
+    }
+
+    public String getMethodName() {
+        return methodName;
+    }
+
+    public String getMethodDescriptor() {
+        return methodDescriptor;
+    }
+
+    public NativeMethod getNativeMethod() {
+        return nativeMethod;
+    }
 }
