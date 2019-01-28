3c3ac599199a5c5d42d14dc34c7e2c9c8900a66f
cgIIrw <chenguo_jay@163.com>
2019-01-28 19:41:50 +0800

    添加测试hashCode()的用例

---
+++ example/HashCodeTest.java
@@ -0,0 +1,6 @@
+public class HashCodeTest {
+    public static void main(String[] args) {
+        Object hashCodeTest = new HashCodeTest();
+        System.out.println(hashCodeTest.hashCode());
+    }
+}
