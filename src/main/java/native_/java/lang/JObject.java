59430dd4ebac3e406baada5d54a872e9a6c85f8a
cgIIrw <chenguo_jay@163.com>
2019-02-15 21:03:27 +0800

    修改JObject，添加注册getClass方法的代码

--- src/main/java/native_/java/lang/JObject.java
+++ src/main/java/native_/java/lang/JObject.java
@@ -3,6 +3,7 @@ package native_.java.lang;
 import native_.JNINativeMethod;
 import native_.JVM_ENTRY.*;
 import native_.Registry;
+import rtda.stack.StackFrame_;
 
 public class JObject {
     private static JNINativeMethod[] methods = {
@@ -11,13 +12,20 @@ public class JObject {
             new JNINativeMethod("notify", "()V", new JVM_MonitorNotify()),
             new JNINativeMethod("notifyAll", "()V", new JVM_MonitorNotifyAll()),
             new JNINativeMethod("clone", "()Ljava/lang/Object;", new JVM_Clone()),
+            new JNINativeMethod("getClass", "()Ljava/lang/Class;", new GetObjectClass()),
     };
 
     // 传入className，对本地方法进行注册
     public static void Java_java_lang_Object_registerNatives(String className) {
         for (JNINativeMethod jniNativeMethod : methods) {
-            Registry.registerMethod(className, jniNativeMethod.getMethodName()
-                    , jniNativeMethod.getMethodDescriptor(), jniNativeMethod.getNativeMethod());
+            Registry.registerMethod(className, jniNativeMethod.getMethodName(),
+                    jniNativeMethod.getMethodDescriptor(), jniNativeMethod.getNativeMethod());
         }
     }
+//
+//    public static void Java_java_lang_Object_getClass(StackFrame_ frame) {
+////        Registry.registerMethod(className, "getClass",
+////                "()Ljava/lang/Class", new GetObjectClass());
+//        new GetObjectClass().execute(frame);
+//    }
 }
