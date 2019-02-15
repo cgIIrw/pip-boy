387c19746615624d6a46b4fd3241c59909072c0b
cgIIrw <chenguo_jay@163.com>
2019-02-15 21:22:40 +0800

    在java.lang包中添加JClass类

--- src/main/java/native_/java/lang/JClass.java
+++ src/main/java/native_/java/lang/JClass.java
@@ -1,4 +1,21 @@
 package native_.java.lang;
 
+import native_.JNINativeMethod;
+import native_.JVM_ENTRY.*;
+import native_.Registry;
+
 public class JClass {
+
+    private static JNINativeMethod[] methods = {
+            new JNINativeMethod("getName0",
+                    "()Ljava/lang/String;", new JVM_GetClassName()),
+    };
+
+    // 传入className，对本地方法进行注册
+    public static void Java_java_lang_Object_registerNatives(String className) {
+        for (JNINativeMethod jniNativeMethod : methods) {
+            Registry.registerMethod(className, jniNativeMethod.getMethodName(),
+                    jniNativeMethod.getMethodDescriptor(), jniNativeMethod.getNativeMethod());
+        }
+    }
 }
