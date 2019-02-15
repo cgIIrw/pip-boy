a1a6c46239e2d78d3234dd3ccc7cde5d2b5ec25e
cgIIrw <chenguo_jay@163.com>
2019-02-15 21:29:03 +0800

    修改INVOKE_NATIVE以便注册JClass的本地方法

--- src/main/java/instructions/natived/INVOKE_NATIVE.java
+++ src/main/java/instructions/natived/INVOKE_NATIVE.java
@@ -3,6 +3,7 @@ package instructions.natived;
 import instructions.base.NoOperandsInstruction;
 import native_.NativeMethod;
 import native_.Registry;
+import native_.java.lang.JClass;
 import rtda.methodarea.Method_;
 import rtda.stack.StackFrame_;
 
@@ -13,11 +14,13 @@ public class INVOKE_NATIVE extends NoOperandsInstruction {
         String className = method.getInstanceKlass_().getThisClassName();
         String methodName = method.getName();
         String methodDescriptor = method.getDescriptor();
+        if (className.equals("java/lang/Class"))
+            JClass.Java_java_lang_Object_registerNatives(className);
         // 查找相应的本地方法
         NativeMethod nativeMethod = Registry.findNativeMethod(className, methodName, methodDescriptor);
         // 如果返回的是null，那么抛出一个带有信息的运行时异常
         if (nativeMethod == null) {
-            String methodInfo = className + "." + methodName + methodDescriptor;
+            String methodInfo = className + "." + methodName + " " + methodDescriptor;
             throw new RuntimeException(methodInfo);
         }
         // 执行本地方法
