b50c27648cf1615a9db120279b4a1384162944f7
cgIIrw <chenguo_jay@163.com>
2019-05-08 23:42:28 +0800

    完成mark函数的编写

--- src/main/kotlin/gc/Gc.kt
+++ src/main/kotlin/gc/Gc.kt
@@ -1,11 +1,53 @@
 package gc
 
+import rtda.heap.Instance_
+import rtda.stack.Slot_
+
 /**
  * @Author: cgrw
  * @Url: https://github.com/cgIIrw/pip-boy
  */
 
+var arr = arrayOfNulls<Slot_>(100)
+
+class Oop_ {
+    var i: Int? = null
+    var size: Int? = null
+    var mark: Boolean = false
+}
+
+var oopMap = HashMap<Instance_, Oop_>()
+
 // 垃圾回收方法
 fun gC() {
+    mark_phase()
+    sweep_phase()
+}
+
+fun mark_phase() {
 
 }
+
+fun sweep_phase() {
+
+}
+
+fun mark(slot: Slot_) {
+    if (slot.ref == null)
+        return
+
+    var oop = oopMap[slot.ref]
+    if (!oop!!.mark) {
+        oop.mark = true
+
+        for (i in oop.i!! until (oop.i!! + oop.size!!)) {
+            if (arr[i]!!.ref != null) {
+                var oop0 = oopMap[arr[i]!!.ref]
+                if (!oop0!!.mark) {
+                    oop0.mark = true
+                }
+            }
+        }
+    }
+}
+
