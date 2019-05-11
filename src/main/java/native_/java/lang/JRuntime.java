915f9b31328b2f3d6808593878a15b98709738b5
cgIIrw <chenguo_jay@163.com>
2019-05-12 01:24:23 +0800

    JRuntime类的实现

--- src/main/java/native_/java/lang/JRuntime.java
+++ src/main/java/native_/java/lang/JRuntime.java
@@ -1,7 +1,21 @@
 package native_.java.lang;
 
+import native_.JNINativeMethod;
+import native_.JVM_ENTRY;
+import native_.Registry;
+
 /**
  * @Author: cgrw
  */
 public class JRuntime {
+    private static JNINativeMethod[] methods = {
+            new JNINativeMethod("gc", "()V", new JVM_ENTRY.JVM_GC()),
+    };
+
+    public static void Java_java_lang_Runtime_registerNatives(String className) {
+        for (JNINativeMethod jniNativeMethod : methods) {
+            Registry.registerMethod(className, jniNativeMethod.getMethodName(),
+                    jniNativeMethod.getMethodDescriptor(), jniNativeMethod.getNativeMethod());
+        }
+    }
 }
